3801c2d640c7f065880480f7cf6376a19653aace
Alvaro Rodrigues <alvarorodrigues1710@gmail.com>
2020-02-25 23:05:37 +0000

    added event binding to view recipe, display pages structures and add new ingredients

--- src/app/recipe-book/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipe-book/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeItemComponent implements OnInit {
 
-  constructor() { }
+  @Input() recipe: { name: string, description: string, imagePath: string };
+  @Output() recipeSelected = new EventEmitter<void>();
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onSelected() {
+    this.recipeSelected.emit();
+  }
 }
